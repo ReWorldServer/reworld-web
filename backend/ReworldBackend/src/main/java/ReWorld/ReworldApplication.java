11b267bed2c1e55fc04ddfa9df7b6e233ddcdbed
Zerritug <Fadeslol222@gmail.com>
2025-11-24 10:59:08 -0500

    Initial commit

---
+++ backend/ReworldBackend/src/main/java/ReWorld/ReworldApplication.java
@@ -0,0 +1,13 @@
+package ReWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReworldApplication.class, args);
+	}
+
+}
---
+++ backend/ReworldBackend/src/test/java/ReWorld/ReworldApplicationTests.java
@@ -0,0 +1,13 @@
+package ReWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
